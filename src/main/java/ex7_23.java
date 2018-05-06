80aa6539add062864dd0cbf882cd07a8d1f827f6
lehade <alexd89@list.ru>
2018-05-06 22:54:06 +0300

    countdown 30 to 0

---
+++ src/main/java/ex7_23.java
@@ -0,0 +1,11 @@
+public class ex7_23 {
+    public static void main(String[] args) throws Exception{
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+            Thread.sleep(100);
+            //напишите тут ваш код
+        }
+
+        System.out.println("Бум!");
+    }
+}
